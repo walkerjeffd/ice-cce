103fd935257143ad76279f5d6e39067ffb32ffa8
Jeff Walker <walker.jeff.d@gmail.com>
2018-10-25 15:13:08 -0400

    add subbasins layer

--- src/components/IceMap.vue
+++ src/components/IceMap.vue
@@ -79,6 +79,11 @@ export default {
         opacity: 1,
         visible: false,
       }, {
+        layer: 'norock:subbasins',
+        label: 'Subbasins',
+        opacity: 1,
+        visible: false,
+      }, {
         layer: 'norock:huc8',
         label: 'HUC8',
         opacity: 1,
